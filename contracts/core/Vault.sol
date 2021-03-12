e9b2d9c7a17a82e05f57ec2a7e281b92623f9033
Phi <phi@lattefi.app>
2021-03-12 15:42:13 +0800

    add vault.updateLong

--- contracts/core/Vault.sol
+++ contracts/core/Vault.sol
@@ -14,20 +14,34 @@ contract Vault is ReentrancyGuard {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
+    struct Position {
+        uint256 size;
+        uint256 entryPrice;
+        uint256 leverage;
+        uint256 reservedAmount;
+    }
+
     uint256 constant BASIS_POINTS_DIVISOR = 10000;
+    uint256 constant MIN_LEVERAGE = 10000;
 
     bool public isInitialized;
 
     address public gusd;
     address public gov;
 
+    uint256 public maxLeverage = 500000; // 50x
+
     mapping (address => bool) public whitelistedTokens;
+    mapping (address => bool) public stableTokens;
     mapping (address => address) public priceFeeds;
     mapping (address => uint256) public pricePrecisions;
     mapping (address => uint256) public redemptionBasisPoints;
     mapping (address => uint256) public tokenDecimals;
 
     mapping (address => uint256) public gusdAmounts;
+    mapping (address => uint256) public reservedAmounts;
+
+    mapping (address => mapping (address => Position)) public longPositions;
 
     modifier onlyGov() {
         require(msg.sender == gov, "GUSD: forbidden");
@@ -65,7 +79,6 @@ contract Vault is ReentrancyGuard {
     }
 
     function sellGUSD(address _token, uint256 _gusdAmount, address _receiver) external nonReentrant {
-        // TODO: validate that assets sent out do not exceed available amounts
         require(_gusdAmount > 0, "Vault: invalid _gusdAmount");
         require(whitelistedTokens[_token], "Vault: _token not whitelisted");
 
@@ -80,12 +93,40 @@ contract Vault is ReentrancyGuard {
 
         gusdAmounts[_token] = gusdAmounts[_token].sub(_gusdAmount);
 
-        IERC20(_token).safeTransfer(_receiver, tokenAmount);
         IGUSD(gusd).burn(msg.sender, _gusdAmount);
+        _transferOut(_token, tokenAmount, _receiver);
+    }
+
+    function updateLong(address _token, uint256 _collateralAmount, uint256 _leverage) external nonReentrant {
+        require(whitelistedTokens[_token], "Vault: _token not whitelisted");
+        require(!stableTokens[_token], "Vault: _token must not be a stableToken");
+        require(_leverage > 0, "Vault: invalid _leverage");
+        require(_leverage > MIN_LEVERAGE, "Vault: _leverage must be more than MIN_LEVERAGE");
+        require(_leverage <= maxLeverage, "Vault: max leverage exceeded");
+
+        uint256 existingCollateral = _closeLongPosition(msg.sender, _token);
+        if (existingCollateral > _collateralAmount) {
+            _transferOut(_token, existingCollateral.sub(_collateralAmount), msg.sender);
+        }
+
+        if (_collateralAmount == 0) { return; }
+
+        if (_collateralAmount > existingCollateral) {
+            _transferIn(_token, _collateralAmount.sub(existingCollateral));
+        }
+
+        uint256 borrowAmount = _collateralAmount.mul(_leverage).div(BASIS_POINTS_DIVISOR);
+        _increaseReservedAmount(_token, borrowAmount);
+
+        longPositions[msg.sender][_token] = Position(
+            tokenToUsdMin(_token, _collateralAmount),
+            getMaxPrice(_token),
+            _leverage,
+            borrowAmount
+        );
     }
 
     function swap(address _tokenIn, address _tokenOut, uint256 _amountIn, address _receiver) external nonReentrant {
-        // TODO: validate that assets sent out do not exceed available amounts
         require(_amountIn > 0, "Vault: invalid _amountIn");
         require(whitelistedTokens[_tokenIn], "Vault: _tokenIn not whitelisted");
         require(whitelistedTokens[_tokenOut], "Vault: _tokenOut not whitelisted");
@@ -96,7 +137,7 @@ contract Vault is ReentrancyGuard {
         gusdAmounts[_tokenOut] = gusdAmounts[_tokenOut].sub(amountOut);
 
         _transferIn(_tokenIn, _amountIn);
-        IERC20(_tokenOut).safeTransfer(_receiver, amountOut);
+        _transferOut(_tokenOut, amountOut, _receiver);
     }
 
     function getMinPrice(address _token) public view returns (uint256) {
@@ -159,10 +200,71 @@ contract Vault is ReentrancyGuard {
         return _amount.mul(10 ** decimalsMul).div(10 ** decimalsDiv);
     }
 
+    function availableReserve(address _token) public view returns (uint256) {
+        uint256 balance = IERC20(_token).balanceOf(address(this));
+        return balance.sub(reservedAmounts[_token]);
+    }
+
+    function tokenToUsdMin(address _token, uint256 _tokenAmount) public view returns (uint256) {
+        uint256 price = getMaxPrice(_token);
+        return _tokenAmount.mul(price);
+    }
+
+    function usdToTokenMin(address _token, uint256 _usdAmount) public view returns (uint256) {
+        uint256 price = getMaxPrice(_token);
+        return _usdAmount.div(price);
+    }
+
     function _transferIn(address _token, uint256 _amount) private {
         uint256 tokenBefore = IERC20(_token).balanceOf(address(this));
-        IERC20(_token).safeTransfer(address(this), _amount);
+        IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
         uint256 tokenAfter = IERC20(_token).balanceOf(address(this));
         require(tokenAfter.sub(tokenBefore) == _amount, "Vault: invalid transfer");
+    }
+
+    function _transferOut(address _token, uint256 _amount, address _receiver) private {
+        uint256 balance = IERC20(_token).balanceOf(address(this));
+        uint256 newBalance = balance.sub(_amount);
+        require(newBalance >= reservedAmounts[_token], "Vault: reserve limit exceeded");
+        IERC20(_token).safeTransfer(_receiver, _amount);
+    }
+
+    function _increaseReservedAmount(address _token, uint256 _amount) private {
+        uint256 balance = IERC20(_token).balanceOf(address(this));
+        reservedAmounts[_token] = reservedAmounts[_token].add(_amount);
+        require(reservedAmounts[_token] <= balance, "Vault: insufficient reserve");
+    }
+
+    function _validateReserveAmount(address _token) private view {
+        uint256 balance = IERC20(_token).balanceOf(address(this));
+        require(reservedAmounts[_token] <= balance, "Vault: reserve limit exceeded");
+    }
+
+    function _closeLongPosition(address _account, address _token) private returns (uint256) {
+        Position storage position = longPositions[_account][_token];
+        if (position.size == 0) {
+            return 0;
+        }
+
+        reservedAmounts[_token] = reservedAmounts[_token].sub(position.reservedAmount);
+        position.size = 0;
+        position.reservedAmount = 0;
+
+        uint256 currentPrice = getMinPrice(_token);
+        bool hasProfit = currentPrice > position.entryPrice;
+        uint256 priceDelta = hasProfit ? currentPrice.sub(position.entryPrice) : position.entryPrice.sub(currentPrice);
+        uint256 delta = position.size.mul(priceDelta).mul(position.leverage).div(position.entryPrice).div(BASIS_POINTS_DIVISOR).div(BASIS_POINTS_DIVISOR);
+
+        if (hasProfit) {
+            uint256 newSize = position.size.add(delta);
+            return usdToTokenMin(_token, newSize);
+        }
+
+        if (delta >= position.size) {
+            return 0; // user's position has been liquidated
+        }
+
+        uint256 newSize = position.size.sub(delta);
+        return usdToTokenMin(_token, newSize);
     }
 }
