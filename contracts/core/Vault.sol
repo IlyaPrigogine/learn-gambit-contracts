ac114a236433e25fb37c9f9df79b5695f9e064a8
Phi <phi@lattefi.app>
2021-03-17 11:33:33 +0800

    restructure margin functions

--- contracts/core/Vault.sol
+++ contracts/core/Vault.sol
@@ -16,9 +16,9 @@ contract Vault is ReentrancyGuard {
 
     struct Position {
         uint256 size;
-        uint256 entryPrice;
-        uint256 leverage;
-        uint256 reservedAmount;
+        uint256 qty;
+        uint256 collateral;
+        uint256 averagePrice;
         uint256 entryFundingRate;
     }
 
@@ -56,8 +56,7 @@ contract Vault is ReentrancyGuard {
     mapping (address => uint256) public cumulativeFundingRates;
     mapping (address => uint256) public feeReserves;
 
-    mapping (bytes32 => Position) public longPositions;
-    mapping (bytes32 => Position) public shortPositions;
+    mapping (bytes32 => Position) public positions;
 
     modifier onlyGov() {
         require(msg.sender == gov, "GUSD: forbidden");
@@ -118,116 +117,53 @@ contract Vault is ReentrancyGuard {
         _transferOut(_token, amountAfterFees, _receiver);
     }
 
-    function updateLong(address _token, uint256 _collateralAmount, uint256 _leverage) external nonReentrant {
-        require(whitelistedTokens[_token], "Vault: _token not whitelisted");
-        require(!stableTokens[_token], "Vault: _token must not be a stableToken");
-        require(_leverage > MIN_LEVERAGE, "Vault: _leverage must be more than MIN_LEVERAGE");
-        require(_leverage <= maxLeverage, "Vault: max leverage exceeded");
-
-        updateCumulativeFundingRate(_token);
-
-        bytes32 key = getLongPositionKey(msg.sender, _token);
-        uint256 existingCollateral = _closeLongPosition(msg.sender, _token);
-
-        if (existingCollateral > _collateralAmount) {
-            uint256 amountOut = _collectMarginFees(_token, existingCollateral.sub(_collateralAmount), longPositions[key].leverage);
-            _transferOut(_token, amountOut, msg.sender);
-        }
-
-        if (_collateralAmount == 0) { return; }
-
-        if (_collateralAmount > existingCollateral) {
-            _transferIn(_token, _collateralAmount.sub(existingCollateral));
-        }
-
-        uint256 amountAfterFees = _collectMarginFees(_token, _collateralAmount, _leverage);
-
-        // if a position remains active, the collateral amount must be more than the
-        // liquidation fee to ensure that the network fee of liquidating the position
-        // is covered
-        uint256 usdAmount = tokenToUsdMin(_token, amountAfterFees);
-        require(usdAmount >= getLiquidationFee(_token));
-
-        uint256 borrowAmount = amountAfterFees.mul(_leverage).div(BASIS_POINTS_DIVISOR);
-        _increaseReservedAmount(_token, borrowAmount);
-
-        // store the position's size in USD as the profits / losses should be calculated
-        // based on the USD value
-        longPositions[key] = Position(
-            usdAmount,
-            getMaxPrice(_token),
-            _leverage,
-            borrowAmount,
-            cumulativeFundingRates[_token]
-        );
-    }
-
-    // _indexToken is intentionally not enforced to be a whitelisted token
-    // it just needs to have a valid priceFeed
-    // this adds flexbility by allowing shorting on non-whitelisted tokens
-    function updateShort(address _collateralToken, address _indexToken, uint256 _collateralAmount, uint256 _leverage) external nonReentrant {
-        require(whitelistedTokens[_collateralToken], "Vault: _collateralToken not whitelisted");
-        require(stableTokens[_collateralToken], "Vault: _collateralToken must be a stableToken");
-        require(_leverage > MIN_LEVERAGE, "Vault: _leverage must be more than MIN_LEVERAGE");
-        require(_leverage <= maxLeverage, "Vault: max leverage exceeded");
-
+    function updatePosition(address _collateralToken, address _indexToken, uint256 _collateral, uint256 _size, bool _isLong) external nonReentrant {
+        _validatePositionTokens(_collateralToken, _indexToken, _isLong);
         updateCumulativeFundingRate(_collateralToken);
 
-        bytes32 key = getShortPositionKey(msg.sender, _collateralToken, _indexToken);
-        uint256 existingCollateral = _closeShortPosition(msg.sender, _collateralToken, _indexToken);
+        bytes32 key = getPositionKey(msg.sender, _collateralToken, _indexToken, _isLong);
+        Position storage position = positions[key];
 
-        if (existingCollateral > _collateralAmount) {
-            uint256 amountOut = _collectMarginFees(_collateralToken, existingCollateral.sub(_collateralAmount), shortPositions[key].leverage);
-            _transferOut(_collateralToken, amountOut, msg.sender);
-        }
+        require(position.size != _size, "Vault: position is unchanged");
 
-        if (_collateralAmount == 0) { return; }
+        bool isBuying = _size > position.size;
+        uint256 sizeDelta = isBuying ? _size.sub(position.size) : position.size.sub(_size);
+        uint256 price = _isLong ? getMaxPrice(_indexToken) : getMinPrice(_indexToken);
 
-        if (_collateralAmount > existingCollateral) {
-            _transferIn(_collateralToken, _collateralAmount.sub(existingCollateral));
-        }
+        uint256 qtyDelta = sizeDelta.div(price);
+        position.qty = isBuying ? position.qty.add(qtyDelta) : position.qty.sub(qtyDelta);
+        position.averagePrice = _size.div(position.qty);
+        position.size = _size;
+        position.entryFundingRate = cumulativeFundingRates[_collateralToken];
 
-        uint256 amountAfterFees = _collectMarginFees(_collateralToken, _collateralAmount, _leverage);
-
-        // if a position remains active, the collateral amount must be more than the
-        // liquidation fee to ensure that the network fee of liquidating the position
-        // is covered
-        uint256 usdAmount = tokenToUsdMin(_collateralToken, amountAfterFees);
-        require(usdAmount >= getLiquidationFee(_collateralToken));
-
-        uint256 borrowAmount = amountAfterFees.mul(_leverage).div(BASIS_POINTS_DIVISOR);
-        _increaseReservedAmount(_collateralToken, borrowAmount);
-
-        // store the position's size in USD as the profits / losses should be calculated
-        // based on the USD value
-        shortPositions[key] = Position(
-            usdAmount,
-            getMaxPrice(_indexToken),
-            _leverage,
-            borrowAmount,
-            cumulativeFundingRates[_collateralToken]
-        );
+        // TODO: handle PnL, funding rate and fees
+        updateCollateral(_collateralToken, _indexToken, _collateral, _isLong);
     }
 
-    function liquidateLong(address _account, address _token, address _feeReceiver) external nonReentrant {
-        updateCumulativeFundingRate(_token);
-        require(shouldLiquidateLong(_account, _token), "Vault: long should not be liquidated");
-
-        _closeLongPosition(_account, _token);
-
-        uint256 feeAmount = getLiquidationFee(_token);
-        _transferOut(_token, feeAmount, _feeReceiver);
-    }
-
-    function liquidateShort(address _account, address _collateralToken, address _indexToken, address _feeReceiver) external nonReentrant {
+    function updateCollateral(address _collateralToken, address _indexToken, uint256 _collateral, bool _isLong) public nonReentrant {
+        _validatePositionTokens(_collateralToken, _indexToken, _isLong);
         updateCumulativeFundingRate(_collateralToken);
-        require(shouldLiquidateShort(_account, _collateralToken, _indexToken), "Vault: short should not be liquidated");
 
-        _closeShortPosition(_account, _collateralToken, _indexToken);
+        bytes32 key = getPositionKey(msg.sender, _collateralToken, _indexToken, _isLong);
+        Position storage position = positions[key];
 
-        uint256 feeAmount = getLiquidationFee(_collateralToken);
-        _transferOut(_collateralToken, feeAmount, _feeReceiver);
+        if (position.collateral == _collateral) {
+            return;
+        }
+
+        if (_collateral > position.collateral) {
+            uint256 delta = _collateral.sub(position.collateral);
+            _transferIn(_collateralToken, usdToTokenMax(_collateralToken, delta));
+        } else {
+            uint256 delta = position.collateral.sub(_collateral);
+            _transferOut(_collateralToken, usdToTokenMin(_collateralToken, delta), msg.sender);
+        }
+
+        position.collateral = _collateral;
+
+        // TODO: validate collateral amount, leverage
     }
+
 
     function getLiquidationFee(address _token) public view returns (uint256) {
         uint256 pricePrecision = getPricePrecision(_token);
@@ -325,73 +261,28 @@ contract Vault is ReentrancyGuard {
         return _usdAmount.div(price);
     }
 
-    function shouldLiquidateLong(address _account, address _token) public view returns (bool) {
-        bytes32 key = getLongPositionKey(_account, _token);
-        Position memory position = longPositions[key];
-        if (position.size == 0) { return false; }
-
-        (bool hasProfit, uint256 delta, uint256 fundingFees) = getLongPositionDelta(_account, _token);
-        return _shouldLiquidate(position.size, hasProfit, delta, fundingFees);
+    function usdToTokenMax(address _token, uint256 _usdAmount) public view returns (uint256) {
+        uint256 price = getMinPrice(_token);
+        return _usdAmount.div(price);
     }
 
-    function shouldLiquidateShort(address _account, address _collateralToken, address _indexToken) public view returns (bool) {
-        bytes32 key = getShortPositionKey(_account, _collateralToken, _indexToken);
-        Position memory position = longPositions[key];
-        if (position.size == 0) { return false; }
-
-        (bool hasProfit, uint256 delta, uint256 fundingFees) = getShortPositionDelta(_account, _collateralToken, _indexToken);
-        return _shouldLiquidate(position.size, hasProfit, delta, fundingFees);
-    }
-
-    function getLongPositionDelta(address _account, address _token) public view returns (bool, uint256, uint256) {
-        bytes32 key = getLongPositionKey(_account, _token);
-        Position memory position = longPositions[key];
-        uint256 currentPrice = getMinPrice(_token);
-        bool priceIncreased = currentPrice > position.entryPrice;
-        uint256 delta;
-        {
-            uint256 priceDelta = priceIncreased ? currentPrice.sub(position.entryPrice) : position.entryPrice.sub(currentPrice);
-            delta = position.size.mul(priceDelta).mul(position.leverage).div(position.entryPrice).div(BASIS_POINTS_DIVISOR).div(BASIS_POINTS_DIVISOR);
-        }
-        uint256 fundingFees;
-        {
-            uint256 fundingRate = cumulativeFundingRates[_token].sub(position.entryFundingRate);
-            fundingFees = fundingRate.mul(position.size).mul(position.leverage).div(BASIS_POINTS_DIVISOR).div(FUNDING_RATE_PRECISION);
-        }
-        return (priceIncreased, delta, fundingFees);
-    }
-
-    function getShortPositionDelta(address _account, address _collateralToken, address _indexToken) public view returns (bool, uint256, uint256) {
-        bytes32 key = getShortPositionKey(_account, _collateralToken, _indexToken);
-        Position memory position = shortPositions[key];
-        uint256 currentPrice = getMaxPrice(_indexToken);
-        bool priceIncreased = currentPrice > position.entryPrice;
-        uint256 delta;
-        {
-            uint256 priceDelta = priceIncreased ? currentPrice.sub(position.entryPrice) : position.entryPrice.sub(currentPrice);
-            delta = position.size.mul(priceDelta).mul(position.leverage).div(position.entryPrice).div(BASIS_POINTS_DIVISOR).div(BASIS_POINTS_DIVISOR);
-        }
-        uint256 fundingFees;
-        {
-            uint256 fundingRate = cumulativeFundingRates[_collateralToken].sub(position.entryFundingRate);
-            fundingFees = fundingRate.mul(position.size).mul(position.leverage).div(BASIS_POINTS_DIVISOR).div(FUNDING_RATE_PRECISION);
-        }
-        return (!priceIncreased, delta, fundingFees);
-    }
-
-    function getLongPositionKey(address _account, address _token) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(
-            _account,
-            _token
-        ));
-    }
-
-    function getShortPositionKey(address _account, address _collateralToken, address _indexToken) public pure returns (bytes32) {
+    function getPositionKey(address _account, address _collateralToken, address _indexToken, bool _isLong) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(
             _account,
             _collateralToken,
-            _indexToken
+            _indexToken,
+            _isLong
         ));
+    }
+
+    function updateCumulativeFundingRate(address _token) public {
+        if (lastFundingTime.add(fundingInterval) > block.timestamp) {
+            return;
+        }
+
+        uint256 fundingRate = getNextFundingRate(_token);
+        cumulativeFundingRates[_token] = cumulativeFundingRates[_token].add(fundingRate);
+        lastFundingTime = block.timestamp.div(fundingInterval).mul(fundingInterval);
     }
 
     function getNextFundingRate(address _token) public view returns (uint256) {
@@ -404,14 +295,14 @@ contract Vault is ReentrancyGuard {
         return fundingRateFactor.mul(reservedAmounts[_token]).mul(intervals).div(balance);
     }
 
-    function updateCumulativeFundingRate(address _token) public {
-        if (lastFundingTime.add(fundingInterval) > block.timestamp) {
-            return;
+    // _indexToken is intentionally not enforced to be a whitelisted token for shorts
+    // it just needs to have a valid priceFeed
+    // this adds flexbility by allowing shorting on non-whitelisted tokens
+    function _validatePositionTokens(address _collateralToken, address _indexToken, bool _isLong) private view {
+        require(whitelistedTokens[_collateralToken], "Vault: _collateralToken not whitelisted");
+        if (_isLong) {
+            require(_collateralToken == _indexToken, "Vault: mismatched tokens");
         }
-
-        uint256 fundingRate = getNextFundingRate(_token);
-        cumulativeFundingRates[_token] = cumulativeFundingRates[_token].add(fundingRate);
-        lastFundingTime = block.timestamp.div(fundingInterval).mul(fundingInterval);
     }
 
     function _collectSwapFees(address _token, uint256 _amount) private returns (uint256) {
@@ -449,69 +340,5 @@ contract Vault is ReentrancyGuard {
     function _validateReserveAmount(address _token) private view {
         uint256 balance = IERC20(_token).balanceOf(address(this));
         require(reservedAmounts[_token] <= balance, "Vault: reserve limit exceeded");
-    }
-
-    function _closeLongPosition(address _account, address _token) private returns (uint256) {
-        bytes32 key = getLongPositionKey(_account, _token);
-        Position storage position = longPositions[key];
-        if (position.size == 0) {
-            return 0;
-        }
-
-        (bool hasProfit, uint256 delta, uint256 fundingFees) = getLongPositionDelta(_account, _token);
-        uint256 nextSize = _getNextPositionSize(_token, position.size, hasProfit, delta, fundingFees);
-
-        feeReserves[_token] = feeReserves[_token].add(usdToTokenMin(_token, fundingFees));
-        reservedAmounts[_token] = reservedAmounts[_token].sub(position.reservedAmount);
-        position.size = 0;
-        position.reservedAmount = 0;
-
-        return nextSize;
-    }
-
-    function _closeShortPosition(address _account, address _collateralToken, address _indexToken) private returns (uint256) {
-        bytes32 key = getShortPositionKey(_account, _collateralToken, _indexToken);
-        Position storage position = shortPositions[key];
-        if (position.size == 0) {
-            return 0;
-        }
-
-        (bool hasProfit, uint256 delta, uint256 fundingFees) = getShortPositionDelta(_account, _collateralToken, _indexToken);
-        uint256 nextSize = _getNextPositionSize(_collateralToken, position.size, hasProfit, delta, fundingFees);
-
-        feeReserves[_collateralToken] = feeReserves[_collateralToken].add(usdToTokenMin(_collateralToken, fundingFees));
-        reservedAmounts[_collateralToken] = reservedAmounts[_collateralToken].sub(position.reservedAmount);
-        position.size = 0;
-        position.reservedAmount = 0;
-
-        return nextSize;
-    }
-
-    function _getNextPositionSize(address _token, uint256 _positionSize, bool _hasProfit, uint256 _delta, uint256 _fundingFees) private view returns (uint256) {
-        if (_hasProfit) {
-            uint256 newSize = _positionSize.add(_delta);
-            return newSize <= _fundingFees ? 0 : usdToTokenMin(_token, newSize.sub(_fundingFees));
-        }
-
-        if (_delta >= _positionSize) {
-            return 0;
-        }
-
-        uint256 newSize = _positionSize.sub(_delta);
-        return newSize <= _fundingFees ? 0 : usdToTokenMin(_token, newSize.sub(_fundingFees));
-    }
-
-    function _shouldLiquidate(uint256 _positionSize, bool _hasProfit, uint256 _delta, uint256 _fundingFees) private pure returns (bool) {
-        if (_hasProfit) {
-            uint256 newSize = _positionSize.add(_delta);
-            return newSize <= _fundingFees;
-        }
-
-        if (_delta >= _positionSize) {
-            return true;
-        }
-
-        uint256 newSize = _positionSize.sub(_delta);
-        return newSize <= _fundingFees;
     }
 }
