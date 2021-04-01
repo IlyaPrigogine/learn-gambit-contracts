b096f2c44aaee9573f6454db00a8fd81f09c779d
Phi <phi@lattefi.app>
2021-04-01 15:17:02 +0800

    update reader

--- contracts/peripherals/Reader.sol
+++ contracts/peripherals/Reader.sol
@@ -10,25 +10,33 @@ import "../core/interfaces/IVault.sol";
 contract Reader {
     using SafeMath for uint256;
 
-    function getTokenBalances(address _account, address[] memory _tokens) public view returns (uint256[] memory) {
+    function getTokenBalances(address _account, address _weth, address[] memory _tokens) public view returns (uint256[] memory) {
         uint256[] memory balances = new uint256[](_tokens.length);
         for (uint256 i = 0; i < _tokens.length; i++) {
-            balances[i] = IERC20(_tokens[i]).balanceOf(_account);
+            address token = _tokens[i];
+            if (token == _weth) {
+                balances[i] = _account.balance;
+                continue;
+            }
+            balances[i] = IERC20(token).balanceOf(_account);
         }
         return balances;
     }
 
-    function getVaultTokenInfo(address _vault, uint256 usdgAmount, address[] memory _tokens) public view returns (uint256[] memory) {
+    function getVaultTokenInfo(address _vault, address _weth, uint256 _usdgAmount, address[] memory _tokens) public view returns (uint256[] memory) {
         uint256 propsLength = 6;
         IVault vault = IVault(_vault);
 
         uint256[] memory amounts = new uint256[](_tokens.length * propsLength);
         for (uint256 i = 0; i < _tokens.length; i++) {
             address token = _tokens[i];
+            if (token == address(0)) {
+                token = _weth;
+            }
             amounts[i * propsLength] = vault.poolAmounts(token);
             amounts[i * propsLength + 1] = vault.reservedAmounts(token);
             amounts[i * propsLength + 2] = vault.usdgAmounts(token);
-            amounts[i * propsLength + 3] = vault.getRedemptionAmount(token, usdgAmount);
+            amounts[i * propsLength + 3] = vault.getRedemptionAmount(token, _usdgAmount);
             amounts[i * propsLength + 4] = vault.getMinPrice(token);
             amounts[i * propsLength + 5] = vault.getMaxPrice(token);
         }
