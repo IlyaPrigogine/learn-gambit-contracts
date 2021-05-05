5e519a4faff63ebe0873659b3647ee56096f5ca1
Phi <phi@lattefi.app>
2021-05-05 21:30:19 +0800

    add scripts

---
+++ scripts/core/setupDistributors.js
@@ -0,0 +1,66 @@
+const { deployContract, contractAt, sendTxn } = require("../shared/helpers")
+const { expandDecimals } = require("../../test/shared/utilities")
+
+async function main() {
+  const usdg = await contractAt("YieldToken", "0x85E76cbf4893c1fbcB34dCF1239A91CE2A4CF5a7")
+  const xgmt = await contractAt("YieldToken", "0xe304ff0983922787Fd84BC9170CD21bF78B16B10")
+  const gmtUsdgFarm = await contractAt("YieldFarm", "0x3E8B08876c791dC880ADC8f965A02e53Bb9C0422")
+  const xgmtUsdgFarm = await contractAt("YieldFarm", "0x68D7ee2A16AB7c0Ee1D670BECd144166d2Ae0759")
+  const tokenDecimals = 18
+  const wbnb = await contractAt("YieldToken", "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c")
+
+  const farms = [
+    {
+      farm: usdg,
+      yieldTrackerIndex: 0,
+      transferAmount: "24.0"
+    },
+    {
+      farm: xgmt,
+      yieldTrackerIndex: 0,
+      transferAmount: "24.0"
+    },
+    {
+      farm: gmtUsdgFarm,
+      yieldTrackerIndex: 1,
+      transferAmount: "13.4"
+    },
+    {
+      farm: xgmtUsdgFarm,
+      yieldTrackerIndex: 1,
+      transferAmount: "29.3"
+    }
+  ]
+
+  for (let i = 0; i < farms.length; i++) {
+    console.log(`---------- ${i} ----------`)
+    const { farm, yieldTrackerIndex, transferAmount } = farms[i]
+    const convertedTransferAmount = ethers.utils.parseUnits(transferAmount, tokenDecimals)
+    console.log("convertedTransferAmount", convertedTransferAmount.toString())
+    const rewardsPerInterval = convertedTransferAmount.div(168)
+    console.log("rewardsPerInterval", rewardsPerInterval.toString())
+
+    const yieldTrackerAddress = await farm.yieldTrackers(yieldTrackerIndex)
+    console.log("yieldTrackerAddress", yieldTrackerAddress)
+    const yieldTracker0 = await contractAt("YieldTracker", yieldTrackerAddress)
+    const distributorAddress = await yieldTracker0.distributor()
+    console.log("distributorAddress", distributorAddress)
+    const distributor = await contractAt("TimeDistributor", distributorAddress)
+    const rewardToken = await distributor.rewardTokens(yieldTracker0.address)
+    console.log("rewardToken", rewardToken)
+    const tokensPerInterval = await distributor.tokensPerInterval(yieldTracker0.address)
+    console.log("tokensPerInterval", tokensPerInterval.toString())
+    const lastDistributionTime = await distributor.lastDistributionTime(yieldTracker0.address)
+    console.log("lastDistributionTime", lastDistributionTime.toString())
+
+    await sendTxn(wbnb.transfer(distributorAddress, convertedTransferAmount), `wbnb.transfer ${i}`)
+    await sendTxn(distributor.setTokensPerInterval(yieldTrackerAddress, rewardsPerInterval), `distributor.setTokensPerInterval ${i}`)
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
---
+++ scripts/core/withdrawFees.js
@@ -0,0 +1,29 @@
+const { deployContract, contractAt, sendTxn } = require("../shared/helpers")
+const { expandDecimals } = require("../../test/shared/utilities")
+
+async function main() {
+  const receiver = { address: "0x9f169c2189A2d975C18965DE985936361b4a9De9" }
+  const vault = await contractAt("Vault", "0xc73A8DcAc88498FD4b4B1b2AaA37b0a2614Ff67B")
+  const gov = await contractAt("Timelock", "0x330EeF6b9B1ea6EDd620C825c9919DC8b611d5d5")
+
+  const btc = { address: "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c" }
+  const eth = { address: "0x2170ed0880ac9a755fd29b2688956bd959f933f8" }
+  const bnb = { address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c" }
+  const busd = { address: "0xe9e7cea3dedca5984780bafc599bd69add087d56" }
+  const usdc = { address: "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d" }
+  const usdt = { address: "0x55d398326f99059fF775485246999027B3197955" }
+
+  const tokens = [btc, eth, bnb, busd, usdc, usdt]
+
+  for (let i = 0; i < tokens.length; i++) {
+    const token = tokens[i]
+    await sendTxn(gov.withdrawFees(vault.address, token.address, receiver.address), `gov.withdrawFees ${i}`)
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
